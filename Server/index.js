44354297af73908de903a5ec0564f1d65421a439
MarwanYoussef22 <actionmon@yahoo.com>
2023-10-03 15:06:18 -0400

    add characters

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,25 @@
+const mongoURI = 'mongodb://localhost:27017';
+const {MongoClient}= require ('mongodb');
+const client = new MongoClient (mongoURI);
+const dbSwapi = 'swapi'
 const express = require ("express");
-
 const app= express();
 
 app.use(express.json());
 
+app.get("/swapi/characters", async (req , res)=> {
+    try {
+        const database = client.db(dbSwapi);
+        const charactersCol = database.collection('characters');
+        const characters = await charactersCol.find().toArray();
+        res.send(characters);
+       //res.send({message: "Hello world"});
+        console.log('Characters: ' +JSON.stringify(characters));
+        await client.close();
+    } catch {
+        
+        console.log ("Error");
+    }
+});
 
-
-application.listen(3001);
+app.listen(3001);
